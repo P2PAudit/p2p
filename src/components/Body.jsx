5594a1121e20a017562b727b0704d93e6e4d15f1
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-02-10 21:08:31 +0530

    updated some button and added cards

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ranger from "../images/ranger.svg"
+import protocol from "../images/protocol.svg"
 
 const Body = () => {
   return (
@@ -11,15 +13,50 @@ const Body = () => {
         Join our 2-Layer-Protection-Plan <br />
         to secure your project
       </div>
-      <div className="auditbutton">
-        <div>
-          Get your Audit Done today.
+      <div className="explorelinks">
+        <div className="auditbutton">
+          <div>
+            Explore Audit Wars
+          </div>
+        </div>
+        <div className="joinbutton">
+          <div>
+            Get protected
+          </div>
         </div>
       </div>
-      <div className="joinbutton">
-        <div>
-          Join Our Crowdsoure Audit
+      <div className="card">
+        <div className="carditem leftcard">
+          <div className="cardicon">
+            <img src={ranger} alt="ranger icon" />
+          </div>
+          <div className="cardtitle">
+            For Rangers
+          </div>
+          <div className="cardtext">
+            Hack DeFi, save users from theft, and get paid doing it. Sign up to Immunefi as a hacker.
+          </div>
+          <div className="cardlink">
+            Sign up
+          </div>
         </div>
+        <div className="carditem rightcard">
+          <div className="cardicon">
+            <img src={protocol} alt="ranger icon" />
+          </div>
+          <div className="cardtitle">
+            For Protocols
+          </div>
+          <div className="cardtext">
+            Secure your project against catastrophic exploits with DeFi's leading security talent.
+          </div>
+          <div className="cardlink">
+            Learn how
+          </div>
+        </div>
+      </div>
+      <div className="upcomingwars">
+        Upcoming Audit Wars
       </div>
     </div>
   )
