992656e3303e4640b28a93b83d408f673d139df3
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-02-10 14:16:34 +0530

    got the routing done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
 import Header from './components/Header'; 
 import Body from './components/Body';
+import Connect from './components/Connect';
+import Leaderboard from './components/Leaderboard';
+import Services from './components/Services';
+import Reports from './components/Reports';
+import Docs from './components/Docs';
+import Register from './components/Register';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <Header />
-        <Body />
-      </div>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <Header />
+              <Routes>
+                <Route path="p2p/" element={<Body />} />
+                <Route path="p2p/connect" element={<Connect />} />
+                <Route path="p2p/leaderboard" element={<Leaderboard />} />
+                <Route path="p2p/services" element={<Services />} />
+                <Route path="p2p/reports" element={<Reports />} />
+                <Route path="p2p/docs" element={<Docs />} />
+                <Route path="p2p/register" element={<Register />} />
+              </Routes>
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import logo from '../images/logop2p.png';
+import { Link } from "react-router-dom";
 
 const header = () => {
   return (
-    <nav>
-      <div className="navbar">
-          <img src={logo} className="logo" alt="logo" />
-          <div className="p2paudit">
-            p2paudit.xyz
-          </div>
-          <div className="sections">
-            <div className="sectionh">Connect</div>
-            <div className="sectionh">LeaderBoard</div>
-            <div className="sectionh">Services</div>
-            <div className="sectionh">Reports</div>
-            <div className="sectionh">Docs</div>
-            <div className="sectionhbtn">
-              <div className="register">
-                Register
+    <>
+      <nav>
+        <div className="navbar">
+            <Link to='/p2p' className="logo">
+              <img src={logo} className="logo" alt="logo" />
+              <div className="p2paudit">
+                p2paudit.xyz
               </div>
+            </Link>
+            <div className="sections">
+              <Link to='/p2p/connect' className="sectionh">Connect</Link>
+              <Link to='/p2p/leaderboard' className="sectionh">LeaderBoard</Link>
+              <Link to='/p2p/services' className="sectionh">Services</Link>
+              <Link to='/p2p/reports' className="sectionh">Reports</Link>
+              <Link to='/p2p/docs' className="sectionh">Docs</Link>
+              <Link to='/p2p/register' className="sectionhbtn">
+                <div className="register">
+                  Register
+                </div>
+              </Link>
             </div>
-          </div>
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </>
   )
 }
 
