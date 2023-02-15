69a8a08e1c64881b6ace8e83b36fff2fae6153d4
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-02-15 21:02:59 +0530

    server updated

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,35 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors");
+require("dotenv").config();
+const faqmodel = require('./models/faq')
 const PORT = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// connecting to cluster in mogodb atlas inserting password and database name
+mongoose.connect("mongodb+srv://p2paudit-xyz:dNs38324Hp1sv8nn@cluster0.mbyp3tj.mongodb.net/p2paudit?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+})
+
+
+app.get('/getfaqs',async (req, res)=> {
+    try {
+        await faqmodel.find({}, (err, result) => {
+            if (err) {
+            res.send(err)
+            } else {
+            res.send(result);
+            }
+        }).clone();
+
+    } catch (err) {
+        console.log("error in faqmodel.find" + err);
+    }
+});
 
 
 app.listen(PORT, (error) =>{
